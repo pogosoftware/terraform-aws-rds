b8740227283692e5b553b97126379b35cff153fe
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2022-04-20 12:37:26 +0200

    Added db_parameter_group module

---
+++ modules/db_parameter_group/main.tf
@@ -0,0 +1,24 @@
+resource "aws_db_parameter_group" "this" {
+  count = var.create ? 1 : 0
+
+  name        = local.name
+  name_prefix = local.name_prefix
+  description = var.description
+  family      = var.family
+
+  dynamic "parameter" {
+    for_each = var.parameters
+
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = lookup(parameter.value, "apply_method", null)
+    }
+  }
+
+  tags = merge({ "Name" = var.name }, var.tags)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
